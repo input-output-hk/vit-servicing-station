e0253ded25d44cfbf4f540654f5aaaf103b6cff0
Enzo Cioppettini <ecioppettini@atixlabs.com>
2022-04-24 23:59:44 -0300

    reorganize full_proposals_info fields

--- vit-servicing-station-lib/migrations/2020-05-22-112032_setup_db/up.sql
+++ vit-servicing-station-lib/migrations/2020-05-22-112032_setup_db/up.sql
@@ -136,20 +136,20 @@ AS
 SELECT
     proposals.*,
     ifnull(reviews_count, 0) as reviews_count,
-    proposal_simple_challenge.proposal_solution,
-    proposal_community_choice_challenge.proposal_brief,
-    proposal_community_choice_challenge.proposal_importance,
-    proposal_community_choice_challenge.proposal_goal,
-    proposal_community_choice_challenge.proposal_metrics,
     voteplans.chain_vote_start_time,
     voteplans.chain_vote_end_time,
     voteplans.chain_committee_end_time,
     voteplans.chain_voteplan_payload,
     voteplans.chain_vote_encryption_key,
     voteplans.fund_id,
+    challenges.challenge_type,
+    proposal_simple_challenge.proposal_solution,
+    proposal_community_choice_challenge.proposal_brief,
+    proposal_community_choice_challenge.proposal_importance,
+    proposal_community_choice_challenge.proposal_goal,
+    proposal_community_choice_challenge.proposal_metrics,
     proposals_voteplans.chain_proposal_index,
     proposals_voteplans.chain_voteplan_id,
-    challenges.challenge_type,
     groups.group_id
 FROM
     proposals
