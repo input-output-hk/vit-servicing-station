ba0474f89b6ab974c35a18b4eaf39512863a805c
Enzo Cioppettini <ecioppettini@atixlabs.com>
2022-04-29 14:54:50 -0300

    remove unwrap in get_voting_power

--- snapshot-service/src/handlers.rs
+++ snapshot-service/src/handlers.rs
@@ -1,17 +1,8 @@
-// use crate::{
-//     db::{models::snapshot::SnapshotEntry, schema::snapshot},
-//     v0::{context::SharedContext, errors::HandleError, result::HandlerResult},
-// };
-// use diesel::prelude::*;
 use crate::SharedContext;
 use jormungandr_lib::crypto::account::Identifier;
 use serde_json::json;
+use warp::http::StatusCode;
 use warp::{Rejection, Reply};
-
-#[derive(Debug)]
-struct InternalError;
-
-impl warp::reject::Reject for InternalError {}
 
 #[tracing::instrument(skip(context))]
 pub async fn get_voting_power(
@@ -19,14 +10,22 @@ pub async fn get_voting_power(
     voting_key: String,
     context: SharedContext,
 ) -> Result<impl Reply, Rejection> {
-    let key = Identifier::from_hex(&voting_key).unwrap();
+    let key = if let Ok(key) = Identifier::from_hex(&voting_key) {
+        key
+    } else {
+        return Ok(warp::reply::with_status(
+            "Invalid voting key",
+            StatusCode::UNPROCESSABLE_ENTITY,
+        )
+        .into_response());
+    };
 
     match context.get_voting_power(tag, key).await {
         Some(entries) => {
             let results: Vec<_> = entries.into_iter().map(|(voting_group, voting_power)| {
             json!({"voting_power": voting_power, "voting_group": voting_group})
         }).collect();
-            Ok(warp::reply::json(&results))
+            Ok(warp::reply::json(&results).into_response())
         }
         None => Err(warp::reject::not_found()),
     }
