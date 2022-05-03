df758dde30d22dc1c4dd2c9d5ad5b9713ce2b8ee
Enzo Cioppettini <ecioppettini@atixlabs.com>
2022-05-03 10:30:34 -0300

    remove old entries before inserting

--- snapshot-service/src/lib.rs
+++ snapshot-service/src/lib.rs
@@ -22,7 +22,29 @@ pub enum Error {
 
 pub type Tag = String;
 type Group = String;
-type TagId = u32;
+
+#[repr(transparent)]
+struct TagId(u32);
+
+impl TagId {
+    const MIN: Self = Self(u32::MIN);
+
+    fn from_be_bytes(bytes: &[u8]) -> Result<Self, Error> {
+        bytes
+            .try_into()
+            .map_err(|_| Error::InternalError)
+            .map(u32::from_be_bytes)
+            .map(Self)
+    }
+
+    fn to_be_bytes(self) -> [u8; 4] {
+        self.0.to_be_bytes()
+    }
+
+    fn next(&self) -> Self {
+        Self(self.0 + 1)
+    }
+}
 
 #[derive(Clone)]
 pub struct SharedContext {
@@ -55,7 +77,9 @@ impl SharedContext {
             return Ok(None);
         };
 
-        let key = {
+        // the fixed part of the key, tag + user, not including the group (or using the empty
+        // group, which is the min, depending on the point of view).
+        let key_prefix = {
             let mut key = [0u8; size_of::<TagId>() + 32usize];
 
             let (tag_part, id_part) = key.split_at_mut(size_of::<TagId>());
@@ -65,19 +89,18 @@ impl SharedContext {
             key
         };
 
-        let key_len = key.len();
-
         let mut result: Vec<(Group, Value)> = vec![];
 
-        for entries in self.entries.range(key..) {
+        for entries in self.entries.range(key_prefix..) {
             let (k, v) = entries?;
 
-            if k[0..key_len] > key[..] {
+            // we are using only a prefix of the actual key, so we want to compare that part only
+            if k[0..key_prefix.len()] > key_prefix[..] {
                 break;
             }
 
-            let group =
-                String::from_utf8(k[key_len..].to_vec()).map_err(|_| Error::InternalError)?;
+            let group = String::from_utf8(k[key_prefix.len()..].to_vec())
+                .map_err(|_| Error::InternalError)?;
 
             let voting_power = Value::from(u64::from_be_bytes(v.as_ref().try_into().unwrap()));
 
@@ -141,6 +164,25 @@ impl UpdateHandle {
         }
 
         let tag_id = if let Some(existing) = self.tags.get(tag)? {
+            // remove all existing entries for this tag so the ones that are not present in the new
+            // input get deleted
+            for entry in self.entries.range(&*existing..) {
+                let (k, _) = entry?;
+
+                // `existing` here is a prefix of the tree's key, since we are going to remove
+                // everything that starts with this tag, we don't need neither the public key nor
+                // the group.
+                //
+                // this is also equivalent to looping in the range(existing..existing+1).
+                if k[0..existing.len()] > *existing {
+                    break;
+                }
+
+                // notice that this uses the same Batch as the inserts, so if the entry exists in
+                // `snapshot` then it will not incur in a delete followed by an insert to the db.
+                batch.remove(k);
+            }
+
             Tag::Existing(existing)
         } else {
             // unwrapping here is fine because the constructor initializes this entry to 0
@@ -182,12 +224,15 @@ impl UpdateHandle {
                         tags.insert(tag.as_bytes(), id)?;
                         seqs.insert(
                             TAG_SEQ_KEY,
-                            &(TagId::from_be_bytes(id.as_ref().try_into().unwrap()) + 1)
+                            &TagId::from_be_bytes(id.as_ref())
+                                .unwrap()
+                                .next()
                                 .to_be_bytes(),
                         )?;
                     }
 
                     entries.apply_batch(&batch)?;
+
                     Ok(())
                 })?;
 
@@ -280,6 +325,65 @@ mod tests {
         assert_eq!(
             &key_1_values[..],
             &rx.get_voting_power(TAG2, &keys[1]).unwrap().unwrap()[..],
+        );
+    }
+
+    #[tokio::test]
+    pub async fn test_snapshot_previous_entries_get_deleted() {
+        const TAG1: &str = "tag1";
+        const TAG2: &str = "tag2";
+
+        let (rx, mut tx) = new_context().unwrap();
+
+        let voting_key = Identifier::from_hex(
+            "0000000000000000000000000000000000000000000000000000000000000000",
+        )
+        .unwrap();
+
+        let inputs = [
+            VoterHIR {
+                voting_key: voting_key.clone(),
+                voting_group: "GROUP1".into(),
+                voting_power: 1.into(),
+            },
+            VoterHIR {
+                voting_key: voting_key.clone(),
+                voting_group: "GROUP2".into(),
+                voting_power: 1.into(),
+            },
+        ];
+
+        tx.update(TAG1, inputs.clone()).await.unwrap();
+        tx.update(TAG2, inputs.clone()).await.unwrap();
+
+        assert_eq!(
+            rx.get_voting_power(&TAG1, &voting_key).unwrap().unwrap(),
+            inputs
+                .iter()
+                .cloned()
+                .map(|hir| (hir.voting_group, hir.voting_power))
+                .collect::<Vec<_>>()
+        );
+
+        tx.update(TAG1, inputs[0..1].to_vec()).await.unwrap();
+
+        assert_eq!(
+            rx.get_voting_power(&TAG1, &voting_key).unwrap().unwrap(),
+            inputs[0..1]
+                .iter()
+                .cloned()
+                .map(|hir| (hir.voting_group, hir.voting_power))
+                .collect::<Vec<_>>()
+        );
+
+        // asserting that TAG2 is untouched, just in case
+        assert_eq!(
+            rx.get_voting_power(&TAG2, &voting_key).unwrap().unwrap(),
+            inputs
+                .iter()
+                .cloned()
+                .map(|hir| (hir.voting_group, hir.voting_power))
+                .collect::<Vec<_>>()
         );
     }
 }
